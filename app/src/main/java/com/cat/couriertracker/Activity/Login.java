146ab735e40ff06ab63763d5564687bb039c5547
siddheswarojha <siddheswarojha23@gmail.com>
2022-02-09 20:57:58 +0530

    login feature & some UI changes

--- app/src/main/java/com/cat/couriertracker/Activity/Login.java
+++ app/src/main/java/com/cat/couriertracker/Activity/Login.java
@@ -2,15 +2,154 @@ package com.cat.couriertracker.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.cat.couriertracker.R;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Login extends AppCompatActivity {
+
+
+    EditText etEmailSignIn,etPasswordSignIn;
+    Button btnLogin,btnGoToSignUp;
+    TextView tvForgotPassword;
+
+    private String APIKey;
+
+    SharedPreferences sharedPreferences;
+    private static final String SHARED_PREF_NAME = "mypref";
+    private static final String KEY_APIKEY = "APIKey";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etEmailSignIn=findViewById(R.id.etEmailSignIn);
+        etPasswordSignIn=findViewById(R.id.etPasswordSignIn);
+        btnGoToSignUp=findViewById(R.id.btnGoToSignUp);
+        btnLogin=findViewById(R.id.btnLogin);
+        tvForgotPassword=findViewById(R.id.tvForgotPassword);
+
+        sharedPreferences = getSharedPreferences(SHARED_PREF_NAME,MODE_PRIVATE);
+
+        String APIkey = sharedPreferences.getString(KEY_APIKEY,null);
+
+        if(APIkey!=null){
+            startActivity(new Intent(Login.this, SearchPackageActivity.class));
+        }
+
+
+        btnGoToSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Login.this,SignUp.class);
+                startActivity(i);
+            }
+        });
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email= etEmailSignIn.getText().toString().trim();
+                String password = etPasswordSignIn.getText().toString().trim();
+
+
+                loginUser(email,password);
+
+            }
+        });
+
+    }
+
+    private void loginUser(String email, String password) {
+        RequestQueue requestQueue = Volley.newRequestQueue(Login.this);
+        String url = "https://courier-application-tracker.herokuapp.com/api/v1/cat/auth/login";
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+
+                Log.d("responseForLogin",response);
+
+
+                APIKey =null;
+                try
+                {
+                    APIKey= response;
+                    if(APIKey.equals(null))
+                    {
+                        Toast.makeText(Login.this,"Enter the details properly!",Toast.LENGTH_SHORT).show();
+                        etEmailSignIn.setError("Your phone number or password is wrong, please enter the correct credentials!");
+                        etEmailSignIn.requestFocus();
+                        etPasswordSignIn.setError("Your phone number or password is wrong, please enter the correct credentials!",null);
+                        etPasswordSignIn.requestFocus();
+
+                    }
+
+                    else
+                    {
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putString(KEY_APIKEY,APIKey);
+                        editor.apply();
+
+                        Intent i = new Intent(Login.this,SearchPackageActivity.class);
+                        startActivity(i);
+                    }
+                } catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+
+
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("errorFound", error.toString());
+            }
+        }) {
+
+//        @Override
+//        public Map<String, String> getHeaders() throws AuthFailureError{
+//        HashMap<String, String> headers = new HashMap<String, String>();
+//        headers.put("Content-Type", "application/json");
+//        headers.put( "charset", "utf-8");
+//        return headers;
+//    }
+
+            @Override
+            protected Map<String, String> getParams()  {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("password",password);
+                params.put("emailAddress",email);
+                return params;
+
+            }
+
+
+        };
+
+        requestQueue.add(stringRequest);
     }
 }
