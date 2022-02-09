095b914fcb5375b3a9f4dc14b1df2b5f4e80b133
siddheswarojha <siddheswarojha23@gmail.com>
2022-02-09 19:18:39 +0530

    User SignUp using volley req

--- app/src/main/java/com/cat/couriertracker/Activity/SignUp.java
+++ app/src/main/java/com/cat/couriertracker/Activity/SignUp.java
@@ -4,12 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.cat.couriertracker.R;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignUp extends AppCompatActivity {
 
@@ -32,12 +46,19 @@ public class SignUp extends AppCompatActivity {
 
 
 
-//        btnSignUp.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//            }
-//        });
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String name = etNameSignUp.getText().toString().trim();
+                String password = etPasswordSignUp.getText().toString().trim();
+                String email = etEmailSignUp.getText().toString().trim();
+
+
+                createAccount(name,password,email);
+
+            }
+        });
 
         tvGoToLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,5 +68,49 @@ public class SignUp extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void createAccount(String name, String password, String email){
+//    {
+        RequestQueue requestQueue = Volley.newRequestQueue(SignUp.this);
+        String url = "https://courier-application-tracker.herokuapp.com/api/v1/cat/auth/userSignUp";
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("responseFromServer",response);
+
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("errorFound", error.toString());
+            }
+        }) {
+
+//        @Override
+//        public Map<String, String> getHeaders() throws AuthFailureError{
+//        HashMap<String, String> headers = new HashMap<String, String>();
+//        headers.put("Content-Type", "application/json");
+//        headers.put( "charset", "utf-8");
+//        return headers;
+//    }
+
+            @Override
+            protected Map<String, String> getParams()  {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("name","name");
+                params.put("password","password");
+                params.put("emailAddress","EmailNew");
+                Log.d("paramsValue", String.valueOf(params));
+                return params;
+
+            }
+
+
+        };
+
+            requestQueue.add(stringRequest);
     }
 }
